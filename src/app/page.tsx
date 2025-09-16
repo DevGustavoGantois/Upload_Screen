14cd292bb835e27e0b9904c0298102cde40254eb
DevGustavoGantois <gugagantois@gmail.com>
2025-09-16 09:31:38 -0300

    feat:Create component login-form.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import { LoginForm } from "@/components/c-login-form";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Page() {
   return (
-    <div>
-  
-    </div>
+    <>
+     <main className="max-w-[1000px] mx-auto">
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Preencha os campos abaixo para entrar na plataforma.</CardDescription>
+          <CardContent>
+            <LoginForm />
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </main> 
+    </>
   );
 }
